670592a5adc24c3d890a151f5c865239e842e8ba
BorisZ-git <ribos@bk.ru>
2017-03-08 00:50:56 +0300

    start HomeWork

---
+++ HomeWork/HomeWork3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Boris Z
+ * Решить задачу с логинами из предыдущего урока,
+ * только логины и пароли считать из файла в массив. 
+ * */
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
