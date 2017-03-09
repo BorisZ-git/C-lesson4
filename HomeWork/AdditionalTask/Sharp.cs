9e5f195056d15f844c834d98188c2af003868149
BorisZ-git <ribos@bk.ru>
2017-03-09 23:26:39 +0300

    add enum Direction\\ add class Sharp\\ add method Draw\\ add HandleKey

--- HomeWork/AdditionalTask/Sharp.cs
+++ HomeWork/AdditionalTask/Sharp.cs
@@ -10,12 +10,38 @@ namespace AdditionalTask
     {
         int x, y;
         char sym;
+        Direction direction;
         public Sharp (int y,int x,char sym)
         {
             this.x = x;
             this.y = y;
             this.sym = sym;
         }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.DownArrow)
+            {
+                direction = Direction.Down;
+            }
+            else if (key == ConsoleKey.UpArrow)
+            {
+                direction = Direction.Up;
+            }
+            else if (key == ConsoleKey.LeftArrow)
+            {
+                direction = Direction.Left;
+            }
+            else if (key == ConsoleKey.RightArrow)
+            {
+                direction = Direction.Right;
+            }
+        }
+
 
     }
 }
