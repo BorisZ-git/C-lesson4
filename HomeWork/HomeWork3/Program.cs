ce98f5d782688a2ed1318f2cf2e97ea29cbb709f
BorisZ-git <ribos@bk.ru>
2017-03-09 14:52:18 +0300

    wrote arrays variables for datafile login and password

--- HomeWork/HomeWork3/Program.cs
+++ HomeWork/HomeWork3/Program.cs
@@ -15,14 +15,29 @@ namespace HomeWork3
     class Program
     {
         static void Main(string[] args)
-        {
-            Properties.Resources.login;
-            Properties.Resources.password;
+        {            
             string login = Console.ReadLine();
             string password = Console.ReadLine();
-
-
+            string[] vLogin = VerData(Properties.Resources.login);
+            string[] vPassword = VerData(Properties.Resources.password);
+            if (login == "1" ) {; }
 
         }
+        static string[] VerData(string filename)
+        {
+            int count = 0;                        
+            StreamReader srVerify = new StreamReader(filename);
+            while (!srVerify.EndOfStream)
+            {
+                srVerify.ReadLine();
+                count++;
+            }
+            string[] verify = new string[count];
+            for (int i = 0; !srVerify.EndOfStream; i++)
+            {
+                verify[i] = srVerify.ReadLine();
+            }
+            return verify;
+        }        
     }
 }
