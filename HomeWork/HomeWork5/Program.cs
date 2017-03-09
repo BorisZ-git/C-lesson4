8fec4f3c8b3d928bf0864b5cdf8f468d43241e08
BorisZ-git <ribos@bk.ru>
2017-03-10 01:12:36 +0300

    finish HomeWork 5

--- HomeWork/HomeWork5/Program.cs
+++ HomeWork/HomeWork5/Program.cs
@@ -27,18 +27,31 @@ namespace HomeWork5
         {
             Random rnd = new Random();
             int UserAnswer = 0;
-            Doubler doubler = new Doubler(rnd.Next(50,100));
+            Doubler doubler = new Doubler(rnd.Next(30,100));
             for (int i = 0; i < 10; i++)
             {
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine($"Необходимо набрать:{doubler.Finish()}\t"
                     + $"сейчас: {doubler.Current()}\t Осталось ходов: {10-i}");
                 Console.WriteLine("Выберите действия: 1.Увеличить число на один\t"+
                     "2.Умножить на два\t3.Сбросить до одного");
+                Console.ResetColor();
                 int.TryParse(Console.ReadLine(), out UserAnswer);
                 if (UserAnswer == 1) doubler.Increase();
                 if (UserAnswer == 2) doubler.Multi();
                 if (UserAnswer == 3) doubler.Reset();
-
+                if (doubler.Current() == doubler.Finish())
+                {
+                    Console.WriteLine($"Вы победили за {i} ходов!");
+                    Console.ReadLine();
+                    break;
+                }
+                if (doubler.Current()>doubler.Finish())
+                {
+                    Console.WriteLine($"Перебор =(");
+                    Console.ReadLine();
+                    break;
+                }
             }
 
         }
